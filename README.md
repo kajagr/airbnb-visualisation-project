c392c27e7440d0c1703adc19a28fa6915d610719
kajagr <kajagros@gmail.com>
2025-12-12 19:44:05 +0100

    inital commit + some preprocessing

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Airbnb Data Visualization Project
+
+This project is dedicated to visualizing Airbnb data, helping to uncover patterns and trends in property listings, prices, availability, and reviews. The goal is to transform raw Airbnb datasets into insightful visualizations for analysis and decision-making.
+
+
+
+
+
---
+++ src/prepare_country_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import json
+from pathlib import Path
+
+RAW_DIR = Path("../data/raw/listings/")
+OUT_DIR = Path("../data/processed")
+OUT_DIR.mkdir(parents=True, exist_ok=True)
+
+# Load cities data from JSON file
+with open(Path("../data/raw/mapping_info/cities_data.json"), "r") as f:
+    cities_data = json.load(f)
+
+cities_data_output = []
+
+for city_info in cities_data:
+    country = city_info["country"]
+    city = city_info["city"]
+    filename = city_info["filename"]
+    
+    df = pd.read_csv(RAW_DIR / filename)
+
+    df["price"] = (
+        df["price"]
+        .astype(str)
+        .str.replace(r"[\$,]", "", regex=True)
+        .astype(float)
+    )
+
+    avg_price = df["price"].mean()
+    count = len(df)
+    # center of the city
+    lat = df["latitude"].mean()
+    lng = df["longitude"].mean()
+
+    cities_data_output.append({
+        "id": city.lower().replace(" ", "_"),
+        "country": country,
+        "city": city,
+        "avg_price": avg_price,
+        "count": count,
+        "lat": lat,
+        "lng": lng,
+    })
+
+# Write to a single JSON file
+with open(OUT_DIR / "cities_statistical_data.json", "w") as f:
+    json.dump(cities_data_output, f, indent=2)
